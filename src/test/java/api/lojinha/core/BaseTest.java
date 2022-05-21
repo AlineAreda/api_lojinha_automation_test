f7f476dec73c78068dbcfab3f205396ca97d9894
Aline Areda <aredaaline@gmail.com>
2022-05-21 11:56:37 -0300

    adicao classe BaseTest e ServerConfig

--- src/test/java/api/lojinha/core/BaseTest.java
+++ src/test/java/api/lojinha/core/BaseTest.java
@@ -1,6 +1,30 @@
 package api.lojinha.core;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import org.aeonbits.owner.ConfigFactory;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+
 public class BaseTest {
 
+    @BeforeAll
+    public static void setup(){
 
+        ServerConfig config = ConfigFactory.create(ServerConfig.class);
+        RestAssured.baseURI = config.BASE_URL();
+        RestAssured.basePath = config.BASE_PATH();
+
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setContentType(ContentType.JSON);
+        RestAssured.requestSpecification = reqBuilder.build();
+
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        resBuilder.expectResponseTime(Matchers.lessThan(config.MAX_TIMEOUT()));
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    }
 }
--- src/test/java/api/lojinha/core/ServerConfig.java
+++ src/test/java/api/lojinha/core/ServerConfig.java
@@ -1,4 +1,17 @@
 package api.lojinha.core;
 
-public interface ServerConfig {
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:properties/${ENV}.properties",
+        "classpath:properties/lojinha.api.properties"})
+public interface ServerConfig extends Config {
+
+    @Key("BASE_URL")
+    String BASE_URL();
+
+    @Key("BASE_PATH")
+    String BASE_PATH();
+
+    @Key("MAX_TIMEOUT")
+    Long MAX_TIMEOUT();
 }
